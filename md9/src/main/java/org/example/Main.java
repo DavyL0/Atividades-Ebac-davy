e239fd0df34f7caf5afb0d7253affb122c4391b9
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-12-19 12:16:12 -0300

    MD9

---
+++ md9/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int valor1 = 1;
+        System.out.println(valor1);
+
+        Double valor2 = Double.valueOf(valor1);
+        System.out.println(valor2);
+    }
+}
