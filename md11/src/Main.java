dd4624e79a7a5e998400a2b3a946e69bb670f099
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2024-12-21 20:54:13 -0300

    md11

---
+++ md11/src/Main.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Project: md11
+ * Package: PACKAGE_NAME
+ * <p>
+ * User: Davy Lopes
+ * Email: davylopes866@gmail.com
+ * Date: 20/12/2024
+ * Time: 21:54
+ * <p>
+ */public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("digite o nome dos usuarios");
+        String nome = sc.nextLine();
+        String[] names = nome.split(",");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        System.out.println("digite o nome dos usuarios com genero");
+        String nomeG = sc.nextLine();
+        String[] namesG = nomeG.split(",");
+
+        List<String> homens = new ArrayList<>();
+        List<String> mulheres = new ArrayList<>();
+
+        for (String name : namesG) {
+            if(name.endsWith("- M")) {
+                homens.add(name.replace("- M", ""));
+            }else if (name.endsWith("- F")){
+                mulheres.add(name.replace("- F", ""));
+            }
+        }
+        System.out.println("Lista de homens:");
+        for (String homem : homens) {
+            System.out.println(homem);
+        }
+        System.out.println("Lista de mulheres:");
+        for (String mulher : mulheres) {
+            System.out.println(mulher);
+        }
+
+    }
+}
---
+++ md11/src/models/Ordenar.java
@@ -0,0 +1,14 @@
+package models;
+
+/**
+ * Project: md11
+ * Package: models
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 20/12/2024
+ * Time: 21:55
+ * <p>
+ */
+public class    Ordenar {
+}
