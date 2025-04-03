fb266c69e53993a36caf71d7af4f49fc73687cb0
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-04-03 16:45:16 -0300

    uupdate

---
+++ untitled/src/main/java/org/davy/Main.java
@@ -0,0 +1,16 @@
+package org.davy;
+
+/**
+ * Project: Default (Template) Project
+ * Package: org.davy
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 03/04/2025
+ * Time: 16:32
+ * <p>
+ */public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
