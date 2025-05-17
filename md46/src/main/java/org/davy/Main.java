d8fd81da26185932d2e61ed46c84ad2452f03136
Davy Lopes <125506032+DavyL0@users.noreply.github.com>
2025-05-16 21:34:58 -0300

    new

---
+++ md46/src/main/java/org/davy/Main.java
@@ -0,0 +1,22 @@
+package org.davy;
+
+/**
+ * Project: Default (Template) Project
+ * Package: org.davy
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 16/05/2025
+ * Time: 21:23
+ * <p>
+ */
+public class Main {
+    public static void main(String[] args) {
+        Recursivo recursivo = new Recursivo();
+        int n1 = 3;
+        int n2 = 5;
+
+        System.out.println(Recursivo.encontraElemento(n1)
+        +" " + Recursivo.encontraElemento(n2));
+    }
+}
---
+++ md46/src/main/java/org/davy/Recursivo.java
@@ -0,0 +1,38 @@
+package org.davy;
+
+import java.util.Arrays;
+
+/**
+ * Project: md46
+ * Package: org.davy
+ * <p>
+ * User: MegaD
+ * Email: davylopes866@gmail.com
+ * Date: 16/05/2025
+ * Time: 21:24
+ * <p>
+ */
+public class Recursivo {
+    private static int MAX = 100;
+    private static final int[] elements = new int[MAX];
+
+
+    static {
+        Arrays.fill(elements, -1);
+        elements[0] = 1;
+        elements[1] = 1;
+    }
+
+    public static int encontraElemento(int n) {
+        return fatorial(n);
+    }
+
+    private static int fatorial(int n) {
+        if (elements[n] != -1) {
+            return  elements[n];
+        }
+
+        elements[n] = n * fatorial(n -1);
+        return elements[n];
+    }
+}
